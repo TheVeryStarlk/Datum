ed61b8a2fe38754615690180e54a128de992034f
TheVeryStarlk <starlkytminecraft@gmail.com>
2024-12-30 02:39:35 +0300

    Implement all packets extracting.

--- Datum.Console/Program.cs
+++ Datum.Console/Program.cs
@@ -1,10 +1,3 @@
 ﻿using Datum.Extractor;
-using Datum.Extractor.Extractors;
 
 var datum = await DatumExtractor.ExtractAsync("1.8", Edition.Java, CancellationToken.None);
-
-Console.WriteLine("b");
-
-var protocol = datum.Protocol;
-
-Console.WriteLine("a");
