e8e77f9aee63cfc4dfb76db90ba9555dadf44959
TheVeryStarlk <starlkytminecraft@gmail.com>
2024-12-31 00:30:54 +0300

    Generate C# classes from packets.

--- Datum.Example/Program.cs
+++ Datum.Example/Program.cs
@@ -1,11 +1,48 @@
-﻿using Datum.Extractor;
+﻿using System.Globalization;
+using System.Text;
+using Datum.Extractor;
 
 var datum = await DatumExtractor.ExtractJavaAsync("1.8", CancellationToken.None);
 
-var block = datum.Block;
+var info = new CultureInfo("en-US", false).TextInfo;
 
-var air = block!.Blocks[0];
+foreach (var pair in datum.Protocol!.Server.Play!)
+{
+    var packet = pair.Value;
+    var name = $"{info.ToTitleCase(packet.Name[7..].Replace("_", " ")).Replace(" ", "")}Packet";
+    var builder = new StringBuilder();
 
-var protocol = datum.Protocol?.Server.Handshake;
+    builder.AppendLine($"internal sealed class {name} : IPacket");
+    builder.AppendLine("{");
+    builder.AppendLine($"    public int Identifier => 0x{pair.Key:X2};");
 
-return;
+    builder.AppendLine();
+
+    foreach (var property in packet.Properties)
+    {
+        var type = property.Type switch
+        {
+            "varint" => "int",
+            "varlong" => "long",
+            "pstring" or "string" => "string",
+            "u16" => "ushort",
+            "u8" => "byte",
+            "i64" => "long",
+            "buffer" => "ReadOnlyMemory<byte>",
+            "i32" => "int",
+            "i8" => "sbyte",
+            "i16" => "short",
+            "f32" => "float",
+            "f64" => "double",
+            "UUID" => "Guid",
+            "bool" => "bool",
+            _ => info.ToTitleCase(property.Type).Replace("_", "")
+        };
+
+        builder.AppendLine($"    public {type} {info.ToTitleCase(property.Name)} {{ get; set; }}");
+    }
+
+    builder.AppendLine("}");
+
+    Console.WriteLine(builder.ToString());
+}
