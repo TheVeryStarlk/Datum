33e5735a7b0a9c7860a15d5c8b03752b7b7ade02
TheVeryStarlk <starlkytminecraft@gmail.com>
2024-12-30 03:10:37 +0300

    Add example project.

---
+++ Datum.Example/Program.cs
@@ -0,0 +1,11 @@
+﻿using Datum.Extractor;
+
+var datum = await DatumExtractor.ExtractAsync("1.8", Edition.Java, CancellationToken.None);
+
+var protocol = datum.Protocol;
+
+var server = protocol!.Server;
+
+var count = protocol.Server.Play.Count;
+
+var play = server.Play.FirstOrDefault(packet => packet.Key is 0x00);
